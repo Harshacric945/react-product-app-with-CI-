90b07e2872513c0ad91ed8d364ac7d07d970cf2b
Harshacric945 <160142718+Harshacric945@users.noreply.github.com>
2025-05-02 02:57:55 +0530

    added baseURL and apiURL for env.js file to bake react at runtime

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const apiUrl = `/api/products`;
+const baseURL = window.REACT_APP_API_BASE_URL || '';
+const apiUrl = `${baseURL}/api/products`;
+
 
 export const getProducts = async () => {
   try {
